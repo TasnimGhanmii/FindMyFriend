d9132b88835a49ee4324fcbadf872a1b1c67b657
tasnim <tasnimghanmi02@gmail.com>
2025-10-19 10:57:52 +0100

    UI & permissions

--- app/src/main/java/tasnimghanmi/grp1/findmyfriend/MainActivity.java
+++ app/src/main/java/tasnimghanmi/grp1/findmyfriend/MainActivity.java
@@ -1,12 +1,18 @@
 package tasnimghanmi.grp1.findmyfriend;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,31 +23,48 @@ import tasnimghanmi.grp1.findmyfriend.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+    Button btn_sms,btn_ecrire;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        btn_sms=findViewById(R.id.btn_sms);
+        btn_ecrire=findViewById(R.id.btn_sdcard);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        btn_sms.setOnClickListener(v->{
+            if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.SEND_SMS)== PackageManager.PERMISSION_GRANTED)
+            {
+                sensSMS("27028755","bonjour");
+            }
+            else
+            {   //demander permission
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SEND_SMS},1);
+            }
+        });
 
-        ActivityCompat.requestPermissions(this,new String[]{
-                Manifest.permission.SEND_SMS,
-                Manifest.permission.READ_SMS,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.POST_NOTIFICATIONS
-        },1);
     }
 
+    public void sensSMS(String numero,String msg)
+    {
+        //envoie msg depuis carte sim1
+        SmsManager manager=SmsManager.getDefault();
+        manager.sendTextMessage(numero,null,msg,null,null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+        if(requestCode==1)
+        {
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "permission accordée", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "permission refusée", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
